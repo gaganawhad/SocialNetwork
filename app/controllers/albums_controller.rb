572eae36abd89588f644c319b4fbf7373f4a699e
Gagan Awhad <regist.gagan@gmail.com>
2010-11-30 17:45:35 -0600

    Added show action to albums controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,4 +2,7 @@ class AlbumsController < ApplicationController
   def index
     @albums = Album.find_all_by_user_id(params[:user_id])
   end
+  def show
+    @album = Album.find(params[:id])
+  end
 end
