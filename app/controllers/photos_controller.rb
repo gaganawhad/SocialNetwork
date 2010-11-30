cf7d4d6adfe4034ddb98dfe4a8ab2f17b11a9e2f
Gagan Awhad <regist.gagan@gmail.com>
2010-11-30 17:56:43 -0600

    Added the photos controller with the corresponding views

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,20 @@
 class PhotosController < ApplicationController
+  def new
+    @photo = Photo.new
+  end
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+  def create
+    @photo = Photo.new(params[:photo])
+    @photo.save
+    redirect_to user_album_photo_path(:user_id => params[:user_id], :album_id => params[:album_id], :id => @photo.id)
+  end
+  def update
+    @photo = Photo.find(params[:id])
+    @photo.update_attributes(params[:photo])
+  end
+  def show
+    @photo = Photo.find(params[:id])
+  end
 end
