676863b12c86781c4d78ceaac519741259b019d1
Gagan Awhad <regist.gagan@gmail.com>
2010-11-30 23:22:12 -0600

    Added some commands in groups controller actions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,16 +1,25 @@
 class GroupsController < ApplicationController
   def index
-  end
-  def show
-  end
-  def edit
-  end
-  def update
+    @groups = Group.all
   end
   def new
+    @group = Group.new
   end
   def create
+    @group = Group.new(params[:group])
+  end
+  def edit
+    @group = Group.find(params[:id])
+  end
+  def update
+    @group = Group.find(params[:id])
+    @group.update_attributes(params[:group])
+  end
+  def show
+    @group = Group.find(params[:id])
   end
   def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
   end
 end
