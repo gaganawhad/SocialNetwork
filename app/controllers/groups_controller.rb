7e763881afbd8c76df493e3598c8ed2c23693e57
Gagan Awhad <regist.gagan@gmail.com>
2010-11-30 18:54:24 -0600

    Added blank RESTful action to groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,16 @@
 class GroupsController < ApplicationController
+  def index
+  end
+  def show
+  end
+  def edit
+  end
+  def update
+  end
+  def new
+  end
+  def create
+  end
+  def destroy
+  end
 end
