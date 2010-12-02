2b9daedaa4416b471b8d3407e2096258eb9cda06
Gagan Awhad <regist.gagan@gmail.com>
2010-12-02 11:50:01 -0600

    Setting up the relationships between topic, group and creator

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ActiveRecord::Base
   has_attached_file :picture
   has_many :memberships
   has_many :members, :through => :memberships, :source => :user
+  has_many :topics
 #--------------------------------------------------
 #   belongs_to :owner, :class_name => "User", :foreign_key => :owner_id #NOTE trying out ownership in memberships itself
 #-------------------------------------------------- 
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,4 @@
 class Topic < ActiveRecord::Base
+  belongs_to :group
+  belongs_to :creator, :class => "User"
 end
