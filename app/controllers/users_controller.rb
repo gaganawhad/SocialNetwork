2290470403d9d7fbb42261523872901d0219e11b
Gagan Awhad <regist.gagan@gmail.com>
2010-12-02 15:15:57 -0600

    Removed the unrequired line

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
   def new
-    @user = User.new
   end
+  
   def edit
     @user = User.find(params[:id])
   end
