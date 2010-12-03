ad9e056481ffa7a61be9b4db50899e9e1d498057
Gagan Awhad <regist.gagan@gmail.com>
2010-12-03 13:02:45 -0600

    Added create and destroy action to the friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,21 @@
 class FriendshipsController < ApplicationController
+  def create
+    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+    if @friendship.save
+      flash[:notice] = "Added friend"
+      redirect_to root_url
+    else
+      flash[:error] = "Error adding the friendship"
+      redirect_to root_url
+    end
+
+    def destroy
+      @friendship = current_user.friendships.find(params[:friend_id])
+      if @friendship.destroy
+        flash[:notice] = "Removed friend"
+      else
+        flash[:error] = "Could not remove friend"
+      end
+      redirect_to root_url
+    end
 end
