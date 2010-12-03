84f36faa2ba59ed05273f125482528f76c452411
Gagan Awhad <regist.gagan@gmail.com>
2010-12-03 14:45:09 -0600

    Added User interface to add and edit new albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -8,4 +8,25 @@ class AlbumsController < ApplicationController
   def new
     @album = current_user.albums.build
   end
+  def edit
+    @album = Album.find(params[:id])
+  end
+  def create
+    @album = current_user.albums.build(params[:album])
+    if @album.save
+      flash[:notice] = "Album was created successfully"
+    else
+      flash[:error] = "Album was not created"
+    end
+    redirect_to user_path(current_user)
+  end
+  def update
+    @album = Album.find(params[:id])
+    if @album.update_attributes(params[:album])
+      flash[:notice] = "Album was updated successfully"
+    else
+      flash[:error] = "Album was not updateed"
+    end
+    redirect_to user_path(current_user)
+  end
 end
