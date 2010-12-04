0ca510eea69f1017ab9d9174be7887d1182aabaa
Gagan Awhad <regist.gagan@gmail.com>
2010-12-04 01:21:43 -0600

    Added small image to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
   belongs_to :album
   has_attached_file :image, 
-                    :styles => {:thumbnails => "200x200#", :show_size => "1024x768>" }
+                    :styles => {:small => "84x84#", :thumbnails => "200x200#", :show_size => "1024x768>" }
   has_many :comments, :as => :commentable
 end
