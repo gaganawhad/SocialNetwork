e7413346036ec95f3b3b8d23c1f08cdc0ca719a9
Gagan Awhad <regist.gagan@gmail.com>
2010-12-04 00:50:01 -0600

    Added one method to the model that will return the address in one string

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ActiveRecord::Base
   has_one :user
+  def one_string
+    one_line = ""
+    [:line_1, :line_2, :city, :state, :zip].each do |field|
+      unless self.send(field).nil? 
+        one_line = one_line + self.send(field) + " "
+      end
+    end
+    one_line
+  end 
 end
