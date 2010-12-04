fe3b7a0b29981131528e03ec8b6024a3e886d7c9
Gagan Awhad <regist.gagan@gmail.com>
2010-12-04 13:16:25 -0600

    added destroy method to Membership

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,2 +1,11 @@
 class MembershipsController < ApplicationController
+  def destroy 
+    @membership = Membership.find(params[:id])
+    if @membership.destroy
+      params[:notice] = "Removed Group"
+    else
+      prams[:error] = "Could not remove group"
+    end
+    redirect_to user_path(current_user)
+  end
 end
