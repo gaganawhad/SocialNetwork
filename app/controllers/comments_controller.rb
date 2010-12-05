01fe1704ee32347e758f58d93bc4441b14191486
Gagan Awhad <regist.gagan@gmail.com>
2010-12-05 01:24:39 -0600

    creating the comments building from the current user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new(params[:comment])
+    @comment = current_user.comments.build(params[:comment])
     if @comment.save
       flash[:notice] = "Comment created"
     else
