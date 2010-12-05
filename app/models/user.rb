0283ebbb3b16885341001c0b4a3f25f1916053e4
Gagan Awhad <regist.gagan@gmail.com>
2010-12-04 20:10:14 -0600

    Set up relation between User and comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,6 +37,7 @@ class User < ActiveRecord::Base
   has_many :friends, :through => :friendships
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => :friend_id
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+  has_many :comments
 #--------------------------------------------------
 #   has_many :owned_groups, :class => "Group" #NOTE Trying out ownership in memberships itself
 #-------------------------------------------------- 
