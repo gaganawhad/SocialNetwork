2d2e72304dbd98485a8e4ef5a623959c23286499
Gagan Awhad <regist.gagan@gmail.com>
2010-12-05 09:46:24 -0600

    Added a way to join a group

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,4 +1,13 @@
 class MembershipsController < ApplicationController
+  def create
+    @membership = current_user.memberships.build(:group_id => params[:id], :owner => false, :admin => false)
+    if @membership.save
+      params[:notice] = "Joined Group"
+    else
+      prams[:error] = "Could not join group"
+    end
+    redirect_to :back
+  end
   def destroy 
     @membership = Membership.find(params[:id])
     if @membership.destroy
