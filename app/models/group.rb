df6a3faa5f7379002aaf8aff343b3217640ec0f0
Gagan Awhad <regist.gagan@gmail.com>
2010-12-05 02:22:34 -0600

    Making profile picture stick to the size

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,13 @@
 class Group < ActiveRecord::Base
   has_attached_file :picture,
-                    :styles => {:profile => "180x220>", :small => "84x84#"}
+                    :styles => {:profile => "180x220#", :small => "84x84#"}
   has_many :memberships
   has_many :members, :through => :memberships, :source => :user
   has_many :topics
 #--------------------------------------------------
 #   belongs_to :owner, :class_name => "User", :foreign_key => :owner_id #NOTE trying out ownership in memberships itself
 #-------------------------------------------------- 
+  def owner
+    self.memberships.find(:first, :conditions => ["owner = true"]).user
+  end
 end
