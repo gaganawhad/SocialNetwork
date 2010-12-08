2f3b93e6d750cce984f72c69e1d24ef30dfb9160
Gagan Awhad <regist.gagan@gmail.com>
2010-12-07 19:24:50 -0600

    Changed the size for showing the picture to about half

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
   belongs_to :album
   has_attached_file :image, 
-                    :styles => {:small => "84x84#", :thumbnail => "200x200#", :show_size => "1024x768>" }
+                    :styles => {:small => "84x84#", :thumbnail => "200x200#", :show_size => "512x384>" }
   has_many :comments, :as => :commentable
 end
