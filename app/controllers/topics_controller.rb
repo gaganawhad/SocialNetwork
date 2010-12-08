a33fd1b144dc40657ecd247d58f0503c777d14ae
Gagan Awhad <regist.gagan@gmail.com>
2010-12-07 21:03:43 -0600

    Making topics work

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,17 @@
 class TopicsController < ApplicationController
+  def new
+    @group = Group.find(params[:group_id])
+    @topic = @group.topics.build
+  end
+  def create
+    @topic = current_user.topics.build(params[:topic])
+    if @topic.save
+      flash[:notice] = "Created new topic"
+    else
+      flash[:error] = "Could not create topic"
+    end
+    redirect_to group_topic_path(:group_id => params[:group_id], :id => @topic.id)
+  end
   def show
     @topic = Topic.find(params[:id])
     @comment = @topic.comments.build
